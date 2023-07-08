ba3df79e0f0e085584d0be2f2de878bc12cf5b37
Anil Kulkarni <anil@terminal.space>
2023-07-07 17:44:28 -0700

    switch over to reverse-proxy

--- cli/nginx_config.py
+++ cli/nginx_config.py
@@ -47,13 +47,6 @@ class NginxConfig(ServiceConfig):
     @classmethod
     def update_files(cls, *, config: Config, secrets: Config, dirs: Dirs):
         fill_template(
-            template=dirs.templates / "nginx.env",
-            dest=dirs.secrets / "nginx" / ".env",
-            config=config,
-            secrets=secrets,
-        )
-
-        fill_template(
             template=dirs.templates / "nginx.conf",
             dest=dirs.config / "nginx" / "default.conf",
             config=config,
@@ -61,14 +54,15 @@ class NginxConfig(ServiceConfig):
         )
 
         fill_template(
-            template=dirs.templates / "nginx_ssl.conf",
-            dest=dirs.config / "nginx" / "ssl.conf",
+            template=dirs.templates / "nginx_dev.conf",
+            dest=dirs.config / "nginx_dev" / "default.conf",
             config=config,
             secrets=secrets,
         )
+
         fill_template(
-            template=dirs.templates / "nginx_dev.conf",
-            dest=dirs.config / "nginx_dev" / "default.conf",
+            template=dirs.templates / "proxy.json",
+            dest=dirs.secrets / "nginx" / "proxy.json",
             config=config,
             secrets=secrets,
         )
@@ -79,98 +73,3 @@ class NginxConfig(ServiceConfig):
             )
         except FileExistsError:
             pass
-
-        made_changes = False
-        if not config.get(["acme", "account_thumbprint"]):
-            made_changes = True
-            acme_email = secrets.get(["acme", "email"]) or ""
-            # Create the initial_acme_config.tar before running the container
-            # otherwise docker will create a folder in its place
-            with open(dirs.secrets / "nginx" / "initial_acme_config.tar", "wb"):
-                pass
-
-            result = subprocess.run(
-                docker_compose_prefix()
-                + [
-                    "--profile",
-                    "prod",
-                    "run",
-                    "--rm",
-                    "--entrypoint",
-                    "/bin/sh",
-                    "nginx",
-                    "-c",
-                    f'''curl https://get.acme.sh | sh -s email="{acme_email}"''',
-                ],
-                timeout=60,
-            )
-            check_result(result)
-
-            result = subprocess.run(
-                docker_compose_prefix()
-                + [
-                    "--profile",
-                    "prod",
-                    "run",
-                    "--rm",
-                    "--entrypoint",
-                    "/root/.acme.sh/acme.sh",
-                    "nginx",
-                    "--server",
-                    "letsencrypt",
-                    "--register-account",
-                ],
-                capture_output=True,
-                timeout=30,
-            )
-            check_result(result)
-            stdout = result.stdout.decode("utf-8")
-            match = re.search(
-                r"ACCOUNT_THUMBPRINT='(.*)'$", result.stdout.decode("utf-8")
-            )
-            if match is None:
-                print(stdout)
-                raise RuntimeError(
-                    "Could not find ACCOUNT_THUMBPRINT in acme.sh --register-account"
-                )
-            config.set(["acme", "account_thumbprint"], match.group(1))
-
-            # Need to generate the template again, with the new certificate
-            fill_template(
-                template=dirs.templates / "nginx.conf",
-                dest=dirs.config / "nginx" / "default.conf",
-                config=config,
-                secrets=secrets,
-            )
-        if (
-            made_changes
-            or not (dirs.secrets / "nginx" / "initial_acme_config.tar").exists()
-        ):
-            # Create the initial_acme_config.tar before running the container
-            # otherwise docker will create a folder in its place
-            with open(dirs.secrets / "nginx" / "initial_acme_config.tar", "wb"):
-                pass
-            result = subprocess.run(
-                docker_compose_prefix()
-                + [
-                    "--profile",
-                    "prod",
-                    "run",
-                    "--rm",
-                    "--entrypoint",
-                    "/bin/tar",
-                    "nginx",
-                    "c",
-                    "-C",
-                    "/root",
-                    "-f",
-                    "-",
-                    ".acme.sh",
-                ],
-                capture_output=True,
-                timeout=30,
-            )
-            check_result(result)
-
-            with open(dirs.secrets / "nginx" / "initial_acme_config.tar", "wb") as f:
-                f.write(result.stdout)
